3e1aaeea4ef385457296dc638f0964b50e71d0f0
datt16 <osatom.4639@gmail.com>
2021-09-27 14:45:07 +0900

    ✨ボトムナビゲーション作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <hello-world />
+  <div>
+    <div class="text-h3">換気状況</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
 export default {
   name: 'Home',
-
-  components: {
-    HelloWorld,
-  },
+  components: {},
 }
 </script>

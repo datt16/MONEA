19b9462d495f683e5c45a37d1a11f51fbaa53baf
datt16 <osatom.4639@gmail.com>
2021-10-12 11:38:19 +0900

    MN-52 :art: デザイン修正

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar app color="transparent" flat height="80">
+    <v-app-bar app color="white" flat height="90">
       <div class="d-flex align-center">
-        <span class="mr-2 text-h6">換気状況</span>
+        <span class="mr-2 text-h6 font-weight-bold">換気状況</span>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn color="blue lighten-5" depressed>
         <span class="mr-2">build:2021/10/04 9:22</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
@@ -20,7 +16,7 @@
     <v-main>
       <v-container>
         <div id="room-switcher" class="pb-3">
-          <div class="text-h5">4J教室</div>
+          <div class="text-h5 ml-3">4J教室</div>
         </div>
         <router-view />
       </v-container>

21589c79973abe019b3de139b8992269fd7ebaf8
ayanopd <f18096@g.ichinoseki.ac.jp>
2021-10-12 11:18:28 +0900

    テキスト調整、補足スペース追加(仮)

--- src/components/AirStatusView.vue
+++ src/components/AirStatusView.vue
@@ -1,25 +1,38 @@
 <template>
   <div id="air-status-view">
     <v-card tile min-width="350px" min-height="200px" color="white">
-        <v-card-title class="text-h5 justify-center">
+        <v-card-title class="text-h4 justify-center mx-auto">
             適切に換気されています。
         </v-card-title>    
-        <v-card-text>
+        <v-card-text class="text-h6 justify-center mx-auto">
           4/4 センサー異常なし
         </v-card-text>
-        <v-card-text >
+         <v-card-text class="text-h6 justify-center mx-auto">
           平均CO2濃度 600未満
         </v-card-text>
+
+        <v-card-actions>
+          <v-btn icon@click="show = !show">
+            <v-icon>
+              {{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+      <v-expand-transition>
+       <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          　HELP
+        </v-card-text>
+       </div>
+      </v-expand-transition>
     </v-card>
   </div>
 </template>
 
-
-
 <script>
 export default {
-  name: 'AirStatusView',
+  name: 'AirStatusView'
 }
 </script>
-
-

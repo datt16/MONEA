47e91acfbed66687e710811aab50de53a1409d4e
datt16 <osatom.4639@gmail.com>
2021-10-12 10:44:45 +0900

    MN-52 :art: ヘッダー調整した

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="transparent" flat height="80">
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="mr-2 text-h6">換気状況</span>
       </div>
 
       <v-spacer></v-spacer>

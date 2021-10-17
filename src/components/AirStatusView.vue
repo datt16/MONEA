781559f8b15b71d34f569a0f2225d22472b90b38
ayanopd <f18096@g.ichinoseki.ac.jp>
2021-10-17 23:21:32 +0900

    エラー修正

--- src/components/AirStatusView.vue
+++ src/components/AirStatusView.vue
@@ -40,16 +40,3 @@ export default {
 }
 </script>
 
-<style>
-@import "./css/styles.css";
-@import url('https://fonts.googleapis.com/css2?family=Kosugi&display=swap');
-#app {
-/* Google fontsで導入したいfontsからfont-familyを取ってくる */
-  font-family: 'Kosugi', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

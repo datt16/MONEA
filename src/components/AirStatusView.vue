131e00e042d06f49e505a87c55db2976f11048e0
ayanopd <f18096@g.ichinoseki.ac.jp>
2021-10-19 09:43:17 +0900

    レイアウト調整

--- src/components/AirStatusView.vue
+++ src/components/AirStatusView.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="air-status-view">
     <v-card tile min-width="350px" min-height="200px" color="white">
-        <v-card-title v-if="ventilation = 1" class="text-h4 justify-center mx-auto">
+        <v-card-title v-if="ventilation = 1" class="text-h6 justify-center mx-auto">
           適切に換気されています。
         </v-card-title>    
         <v-card-title v-else class="text-h4 justify-center mx-auto">
@@ -20,7 +20,7 @@
         <v-card-text v-else-if="sensor = 1" class="text-h6 justify-center mx-auto">
           1/4 センサー異常あり
         </v-card-text>
-        <v-card-text v-else-if="sensor = 0" class="text-h6 justify-center mx-auto">
+        <v-card-text v-else class="text-h6 justify-center mx-auto">
           0/4 センサー異常あり
         </v-card-text>
 
@@ -40,9 +40,9 @@ export default {
   name: 'AirStatusView',
   data(){
     return{
-      ventilation:1,
-      sensor:3,
-      avarage:600
+      ventilation:0,
+      sensor:1,
+      avarage:500
     }
   }
 }

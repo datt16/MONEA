f2b04793fee991be20551d3a4d720b8f6431dfcd
ayanopd <f18096@g.ichinoseki.ac.jp>
2021-10-19 10:46:03 +0900

    レイアウト、条件分岐修正

--- src/components/AirStatusView.vue
+++ src/components/AirStatusView.vue
@@ -1,35 +1,35 @@
 <template>
   <div id="air-status-view">
     <v-card tile min-width="350px" min-height="200px" color="white">
-        <v-card-title v-if="ventilation = 1" class="text-h5 justify-center">
+        <v-card-title v-if="ventilation === 1" class="text-h5 justify-center">
           適切に換気されています。
         </v-card-title>    
         <v-card-title v-else class="text-h5 justify-center">
           換気が必要です。
         </v-card-title> 
 
-        <v-card-text v-if="sensor === 4" class="justify-center">
+        <p v-if="sensor === 4" class="text-subtitle-1 justify-center">
           4/4 センサー異常なし
-        </v-card-text>
-        <v-card-text v-else-if="sensor === 3" class="justify-center">
+        </p>
+        <p v-else-if="sensor === 3" class="text-subtitle-1 justify-center">
           3/4 センサー異常あり
-        </v-card-text>
-        <v-card-text v-else-if="sensor === 2" class="justify-center">
+        </p>
+        <p v-else-if="sensor === 2" class="text-subtitle-1justify-center">
           2/4 センサー異常あり
-        </v-card-text>
-        <v-card-text v-else-if="sensor === 1" class="justify-center">
+        </p>
+        <p v-else-if="sensor === 1" class="text-subtitle-1 justify-center">
           1/4 センサー異常あり
-        </v-card-text>
-        <v-card-text v-else class="justify-center">
+        </p>
+        <p v-else class="text-subtitle-1 justify-center">
           0/4 センサー異常あり
-        </v-card-text>
+        </p>
 
-        <v-card-text v-if="average < 600" class="justify-center">
+        <p v-if="average < 600" class="text-subtitle-1 justify-center">
           平均CO2濃度 600未満
-        </v-card-text>
-        <v-card-text v-else class="justify-center">
+        </p>
+        <p v-else class="text-subtitle-1 justify-center">
           平均CO2濃度 600以上
-        </v-card-text>
+        </p>
 
     </v-card>
   </div>
@@ -42,7 +42,7 @@ export default {
     return{
       ventilation:0,
       sensor:1,
-      avarage:500
+      avarage:500,
     }
   }
 }

e85f6b2ed630e71c19bc93f0cb0a2b571bc6b839
ayanopd <f18096@g.ichinoseki.ac.jp>
2021-10-26 09:10:09 +0900

    アイコン追加

--- src/components/AirStatusView.vue
+++ src/components/AirStatusView.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="air-status-view">
     <v-card tile min-width="350px" min-height="200px" color="white">
-        <v-card-title v-if="ventilation === 1" class="text-h5 justify-center mb-1">
-          適切に換気されています。
+        <v-icon large v-if="sensor === 4" color="LIME" class="mr-auto">mdi-Check-Bold</v-icon>
+        <v-icon large v-if="sensor >= 3" color="RED" class="mr-auto">mdi-Close-Thick</v-icon>
+        <v-card-title v-if="ventilation === 1" class="text-h5 justify-center mt-5">
+          適切に換気されています
         </v-card-title>    
-        <v-card-title v-else-if="ventilation === 0" class="text-h5 justify-center mb-1">
-          換気が必要です。
+        <v-card-title v-else-if="ventilation === 0" class="text-h5 justify-center mt-5">
+          換気が必要です
         </v-card-title> 
 
+        <v-icon v-if="sensor === 4" color="LIME" class="mr-auto">mdi-Circle-Outline</v-icon>
+        <v-icon v-if="sensor >= 3" color="RED" class="">mdi-Close-Thick</v-icon>
         <p v-if="sensor === 4" class="text-subtitle-1 text-center">
           4/4 センサー異常なし
         </p>
@@ -24,6 +28,8 @@
           0/4 センサー異常あり
         </p>
 
+        <v-icon v-if="sensor === 4" color="LIME" class="mr-auto">mdi-Circle-Outline</v-icon>
+        <v-icon v-if="sensor >= 3" color="RED" class="mr-auto">mdi-Close-Thick</v-icon>
         <p v-if="average >= 600" class="text-subtitle-1 text-center">
           平均CO2濃度 600以上
         </p>
@@ -47,6 +53,7 @@ export default {
   }
 }
 
-
 </script>
+
+
 

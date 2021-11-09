747174c9e4d207cbe65d54f293ca30e806167854
datt16 <osatom.4639@gmail.com>
2021-11-09 09:36:48 +0900

    app.vue移植

--- monea-nuxt/layouts/default.vue
+++ monea-nuxt/layouts/default.vue
@@ -1,117 +1,85 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app>
+    <!-- Appbar -->
+    <div id="appBar">
+      <v-app-bar app flat height="70">
+        <div class="d-flex align-center">
+          <span class="mr-2 text-h6 font-weight-bold">{{ title }}</span>
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-avatar color="primary" size="36">
+          <v-icon dark> mdi-account-circle </v-icon>
+        </v-avatar>
+      </v-app-bar>
+    </div>
+    <!-- __Appbar -->
+
     <v-main>
       <v-container>
+        <!-- roomSwitcher -->
+        <div id="roomSwicther">
+          <v-menu offset-y dark>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="transparent" class="pa-0" v-bind="attrs" v-on="on">
+                <div class="text-h5">4J教室</div>
+                <v-icon>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list dense dark>
+              <v-list-item link>
+                <v-icon left dense>mdi-home</v-icon>
+                <v-list-item-content>
+                  <v-list-item-title> 4J教室 </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+        <!-- __roomSwitcher -->
+
+        <!-- Nuxt ... 実質RouterView -->
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+
+    <!-- BottomNav -->
+    <div id="bottomNavigation">
+      <v-bottom-navigation :value="nav" color="primary" grow fixed shift>
+        <v-btn to="/history" icon style="height: 100%">
+          <span>History</span>
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn to="/" icon style="height: 100%">
+          <span>Home</span>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn to="/actions" icon style="height: 100%">
+          <span>Actions</span>
+
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </div>
+    <!--  __BottomNav -->
+
+    <!-- <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      // fixed: false,
+      title: 'Vuetify.js',
+      nav: 1,
     }
-  }
+  },
 }
 </script>

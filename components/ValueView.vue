58213af5db058da4496e0796f1652957bf2cbbfe
f18006 <f18006@g.ichinoseki.ac.jp>
2021-12-05 20:36:19 +0900

    icon

--- components/ValueView.vue
+++ components/ValueView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card min-height="100px" outlined>
     <v-col>    
-      <p class="text-h7 text-center"><v-icon small>mdi-cloud-outline</v-icon>二酸化炭素濃度</p>
+      <p class="text-h7 text-center"><v-icon small>mdi-cloud</v-icon>二酸化炭素濃度</p>
       <strong class="text-h3 font-weight-bold display-" >{{ sensorppm }}</strong>
       <strong class="text-h5">ppm</strong>
     </v-col>
